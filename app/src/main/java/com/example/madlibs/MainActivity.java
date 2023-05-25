91094663bbceccd83a0d37decd191181b8eaaf93
201009328 <201009328@lbschools.net>
2023-05-25 11:50:03 -0700

    Soundboard button

--- app/src/main/java/com/example/madlibs/MainActivity.java
+++ app/src/main/java/com/example/madlibs/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.madlibs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnDog;
+    private Button btnRain;
+    private Button btnBee;
+    private Button btnCow;
+    private Button btnSheep;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
